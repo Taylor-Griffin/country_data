16f531a91fa39f2f826367a8cb5e0b2b60164da1
Taylor Griffin <tmgriffin73@gmail.com>
2022-06-30 13:01:36 -0400

    complete step1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then((res) => setCountries(res.data));
+  }, []);
+  console.log(countries[1]);
+  const filteredCountries = countries.filter((country) =>
+    country.name.common.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <label htmlFor="countries">Find countries: </label>
+      <input
+        type="text"
+        id="countries"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      {filteredCountries.length > 10 && (
+        <p>Too many matches, please narrow your search</p>
+      )}
+      {filteredCountries.length < 10 &&
+        filteredCountries.length !== 1 &&
+        filteredCountries.map((country) => (
+          <p key={country.name.common}>{country.name.common}</p>
+        ))}
+      {filteredCountries.length === 1 &&
+        filteredCountries.map((country) => (
+          <>
+            <h2>{country.name.common}</h2>
+            <p>Capital:{country.capital}</p>
+            <p>Area: {country.area}</p>
+            <br />
+            <h3>Languages:</h3>
+            <ul>
+              {Object.values(country.languages).map((language) => (
+                <li>{language}</li>
+              ))}
+            </ul>
+            <img
+              src={country.flags.png}
+              alt={`Flag of ${country.name.common}`}
+            />
+          </>
+        ))}
+    </>
   );
 }
 
